e94dab873dba2955764ef6b5870bf1c20bbf43d1
FahimaGold <df_mokhtari@esi.dz>
2023-11-12 11:23:37 +0100

    remove FiberDebugger

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <FiberDebugger/>
+      
       <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
--- src/components/FiberDebugger.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useEffect } from "react";
-
-export function FiberDebugger(){
-    useEffect(() => {
-        console.log('Fiber tree:', React.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner);
-      });
-      return <div>Hello from FiberDebugger</div>;
-}
